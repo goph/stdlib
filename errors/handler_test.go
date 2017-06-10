80dd3386f14a08a6a0dc77c4f553ed7864d3cb6a
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-06-10 20:10:45 +0200

    Remove logrus dependency

--- errors/handler_test.go
+++ errors/handler_test.go
@@ -4,19 +4,25 @@ import (
 	"testing"
 
 	"fmt"
-
-	logrus "github.com/sirupsen/logrus/hooks/test"
 )
 
-func TestLogHandler_Handle_Logrus(t *testing.T) {
-	logger, hook := logrus.NewNullLogger()
+type testErrorLogger struct {
+	last error
+}
+
+func (l *testErrorLogger) Error(args ...interface{}) {
+	l.last = args[0].(error)
+}
+
+func TestLogHandler_Handle(t *testing.T) {
+	logger := &testErrorLogger{}
 	handler := NewLogHandler(logger)
 
 	err := fmt.Errorf("internal error")
 
 	handler.Handle(err)
 
-	if got, want := hook.LastEntry().Message, "internal error"; got != want {
+	if got, want := logger.last, err; got != want {
 		t.Fatalf("expected to log a specific error, received: %v", got)
 	}
 }
