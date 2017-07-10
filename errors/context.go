93fcf0bcc9f7d50642fedf5b856fae45c5681077
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-07-10 22:51:00 +0200

    Add With to append context to errors

--- errors/context.go
+++ errors/context.go
@@ -1,6 +1,61 @@
 package errors
 
+import "errors"
+
 // ContextualError represents an error which holds a context.
 type ContextualError interface {
-	Context() map[string]interface{}
+	Context() []interface{}
+}
+
+// The implementation bellow is heavily influenced by go-kit's log context.
+
+// ErrMissingValue is appended to keyvals slices with odd length to substitute
+// the missing value.
+var ErrMissingValue = errors.New("(MISSING)")
+
+// With returns a new error with keyvals context appended to it.
+// If the wrapped error is already a contextual error created by With
+// keyvals is appended to the existing context, but a new error is returned.
+func With(err error, keyvals ...interface{}) error {
+	if len(keyvals) == 0 {
+		return err
+	}
+
+	var kvs []interface{}
+
+	if c, ok := err.(*contextualError); ok {
+		err = c.err
+		kvs = c.keyvals
+	}
+
+	kvs = append(kvs, keyvals...)
+
+	if len(kvs)%2 != 0 {
+		kvs = append(kvs, ErrMissingValue)
+	}
+	return &contextualError{
+		err: err,
+		// Limiting the capacity of the stored keyvals ensures that a new
+		// backing array is created if the slice must grow in With.
+		// Using the extra capacity without copying risks a data race.
+		keyvals: kvs[:len(kvs):len(kvs)],
+	}
+}
+
+// contextualError is the ContextualError implementation returned by With.
+//
+// It wraps an error and a holds keyvals as the context.
+type contextualError struct {
+	err     error
+	keyvals []interface{}
+}
+
+// Error calls the underlying error and returns it's message.
+func (e *contextualError) Error() string {
+	return e.err.Error()
+}
+
+// Context returns the appended keyvals.
+func (e *contextualError) Context() []interface{} {
+	return e.keyvals
 }
---
+++ errors/context_test.go
@@ -0,0 +1,59 @@
+package errors_test
+
+import (
+	"testing"
+
+	"github.com/goph/stdlib/errors"
+)
+
+func TestContext(t *testing.T) {
+	t.Parallel()
+
+	err := errors.New("")
+
+	kvs := []interface{}{"a", 123}
+	err = errors.With(err, kvs...)
+	kvs[1] = 0 // With should copy its key values
+
+	cerr, ok := err.(errors.ContextualError)
+
+	if !ok {
+		t.Fatal("expected error to implement ContextualError")
+	}
+
+	ctx := cerr.Context()
+
+	if want, have := "a", ctx[0]; want != have {
+		t.Errorf("\nwant: %s\nhave: %v", want, have)
+	}
+
+	if want, have := 123, ctx[1]; want != have {
+		t.Errorf("\nwant: %d\nhave: %v", want, have)
+	}
+}
+
+func TestContext_MissingValue(t *testing.T) {
+	t.Parallel()
+
+	err := errors.New("")
+
+	err = errors.With(err, "k0")
+
+	cerr, ok := err.(errors.ContextualError)
+
+	if !ok {
+		t.Fatal("expected error to implement ContextualError")
+	}
+
+	ctx := cerr.Context()
+
+	if want, have := 2, len(ctx); want != have {
+		t.Errorf("want len(output) == %v, have %v", want, have)
+	}
+
+	for i := 1; i < 2; i += 2 {
+		if want, have := errors.ErrMissingValue, ctx[i]; want != have {
+			t.Errorf("want output[%d] == %#v, have %#v", i, want, have)
+		}
+	}
+}
