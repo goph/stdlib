19908f1bc664e4b28d320c63b2aa904b97a9a304
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-08-23 04:38:03 +0200

    Fix typo

--- stdlib.go
+++ stdlib.go
@@ -2,7 +2,7 @@
 package stdlib
 
 // Must checks if a parameter is an error and panics if so.
-// Useful when you want to force a call to suceed.
+// Useful when you want to force a call to succeed.
 func Must(err error) {
 	if err != nil {
 		panic(err)
