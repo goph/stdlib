0d7a5a09ce42875aa10e69fd74775bfa458a8f0c
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2018-06-03 18:30:51 +0200

    Move MySQL datetime format string to a separate file

---
+++ time/mysql.go
@@ -0,0 +1,4 @@
+package time
+
+// MySQL datetime format
+const MySQLDateTime = "2006-01-02 15:04:05"
--- time/time.go
+++ time/time.go
@@ -1,7 +1,2 @@
 // Package time extends the time package in the stdlib.
 package time
-
-// Time format extensions for the ones in the standard library.
-const (
-	MySQLDateTime = "2006-01-02 15:04:05"
-)
